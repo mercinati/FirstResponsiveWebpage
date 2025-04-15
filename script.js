df285ab37e2643d7eefab62b5d37d42bd3f1e021
Karan Gupta <119480406+mercinati@users.noreply.github.com>
2025-04-15 15:55:13 +0530

    added script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+let signupbutton = document.querySelector(".Sign-up");
+let formcontainer = document.querySelector(".form-container");
+
+
+const formVisible = () => {
+    formcontainer.style.visibility = "visible";
+}
+const formHide = () => {
+    formcontainer.style.visiblity = "hidden";
+}
